b9a151d5757d89fd80c920688cd560c09b456eb0
stimorolmenthol <stimorolmenthol6153@gmail.com>
2017-10-29 19:06:56 +0300

    Tattoobrands project

---
+++ script.js
@@ -0,0 +1,32 @@
+$(function() {
+   if ($('.slider').length > 0) {
+      $('.slider').bxSlider(
+         {
+            mode: 'vertical',
+            controls: false,
+            auto: true,
+            pause: 2000
+         }
+      );
+   }
+   
+   $('.product-img-wrapper').zoom({url: 'img/grommets.jpg'});
+   
+   $('.add-to-cart .plus').click(function () {
+      var current_value = parseInt($('#add-to-cart-input').val());
+      $('#add-to-cart-input').val(current_value + 1);
+      return false;
+   });
+   
+   $('.add-to-cart .minus').click(function () {
+      var current_value = parseInt($('#add-to-cart-input').val());
+      
+      if (current_value <= 1) {
+         return false;
+      }
+      
+      $('#add-to-cart-input').val(current_value - 1);
+      return false;
+   });
+}
+);
